f3d2826b0a7eb5d1a5a2624b90c01edd7a46d9bd
Acasune <tttt4aaaa@gmail.com>
2022-07-18 11:44:47 +0900

    01

---
+++ src/01.rs
@@ -0,0 +1,25 @@
+fn main() {
+    use std::collections::BTreeMap;
+    let mut map = BTreeMap::<usize, Vec<i32>>::new();
+
+    // Preparation
+    for i in 1..=5 {
+        let mut vec = vec![];
+        for j in 1..=3 {
+            vec.push((i * j) as i32);
+        }
+        map.insert(2 * i, vec);
+    }
+    // [Before] map: {2: [1, 2, 3], 4: [2, 4, 6], 6: [3, 6, 9], 8: [4, 8, 12], 10: [5, 10, 15]}
+
+    // Replacing procedure
+    for i in 1..=5 {
+        let idx = 2 * i - 1;
+        if let Some((&key, _)) = map.range((idx)..).next() {
+            let val = map.remove(&key).unwrap();
+            map.insert(idx, val);
+        }
+    }
+    // [After] map {1: [1, 2, 3], 3: [2, 4, 6], 5: [3, 6, 9], 7: [4, 8, 12], 9: [5, 10, 15]}
+    println!("{:?}", map);
+}
